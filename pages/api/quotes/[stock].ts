2aabc620cced8ad3086d7e47971776d0580739cd
Marcos Wade <marcoswade@gmail.com>
2022-11-11 15:39:29 -0400

    add gitignore

--- pages/api/quotes/[stock].ts
+++ pages/api/quotes/[stock].ts
@@ -1,10 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 const finnhub = require('finnhub');
-const apiToken = process.env.QUOTES_API;
+const apiToken = process.env.QUOTES_API_KEY;
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = apiToken;
 const finnhubClient = new finnhub.DefaultApi();
+
 
 export const config = {
   api: {
